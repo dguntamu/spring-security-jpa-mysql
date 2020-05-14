4572455d7dccd81f130fe2e01edd48f1911b639d
Dhananjaya Rao G <dhanu.hyd@gmail.com>
2020-05-14 08:52:10 +0530

    Spring security example initailly hardcoded with user details for authentication and autherisation

---
+++ src/main/java/com/spirng/springsecurityjpamysql/MyUserDetails.java
@@ -0,0 +1,62 @@
+package com.spirng.springsecurityjpamysql;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class MyUserDetails implements UserDetails{
+	private String userName;
+	
+	public MyUserDetails(String userName){
+		this.userName = userName;
+	}
+	
+	public MyUserDetails(){
+		
+	}
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		System.out.println("----------------------");
+		return Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
+	}
+
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return "pass";
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return this.userName;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+}
